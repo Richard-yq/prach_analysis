46d46a9af07f8a27fd6e9447f6b85f33de7bf8a6
Richard-yq <a2311496a@gmail.com>
2025-11-14 13:42:53 +0800

    1114 COTS UE data

---
+++ 1114/data.py
@@ -0,0 +1,79 @@
+DATA = """
+# MTKUE 1114 PrachCFID:148 B4 log
+SFN/SFN/SLOT/SYMBOL,attempt time,preamble index,Tx power
+887|9|1|0,1,1,-27 dbm
+889|9|1|0,2,3,-25 dbm
+891|9|1|0,3,13,-23 dbm
+893|9|1|0,4,47,-21 dbm
+895|9|1|0,5,17,-19 dbm
+897|9|1|0,6,27,-17 dbm
+899|9|1|0,7,25,-15 dbm
+901|9|1|0,8,59,-13 dbm
+903|9|1|0,9,1,-11 dbm
+905|9|1|0,10,43,-9 dbm
+907|9|1|0,11,37,-7 dbm
+909|9|1|0,12,35,-5 dbm
+911|9|1|0,13,29,-3 dbm
+913|9|1|0,14,3,-1 dbm
+915|9|1|0,15,1,1 dbm
+917|9|1|0,16,3,3 dbm
+919|9|1|0,17,5,5 dbm
+921|9|1|0,18,7,7 dbm
+
+
+SFN/SFN/SLOT/SYMBOL,attempt time,preamble index,Tx power
+905|9|1|0,1,12,-27 dbm
+907|9|1|0,2,24,-25 dbm
+909|9|1|0,3,56,-23 dbm
+911|9|1|0,4,48,-21 dbm
+913|9|1|0,5,39,-19 dbm
+915|9|1|0,6,36,-17 dbm
+917|9|1|0,7,33,-15 dbm
+919|9|1|0,8,56,-13 dbm
+921|9|1|0,9,56,-11 dbm
+923|9|1|0,10,18,-9 dbm
+925|9|1|0,11,22,-7 dbm
+927|9|1|0,12,58,-5 dbm
+929|9|1|0,13,58,-3 dbm
+931|9|1|0,14,58,-1 dbm
+933|9|1|0,15,46,1 dbm
+935|9|1|0,16,54,3 dbm
+937|9|1|0,17,14,5 dbm
+939|9|1|0,18,26,7 dbm
+941|9|1|0,19,21,9 dbm
+
+
+# MTKUE 1114 PrachCFID:146 B4 log
+
+SFN/SFN/SLOT/SYMBOL,attempt time,preamble index,Tx power
+329|9|0|0,1,7,-27 dbm
+337|9|0|0,2,53,-25 dbm
+345|9|0|0,3,50,-23 dbm
+353|9|0|0,4,0,-21 dbm
+361|9|0|0,5,8,-19 dbm
+
+# MTKUE 1114 PrachCFID:146 B4 log
+
+SFN/SFN/SLOT/SYMBOL,attempt time,preamble index,Tx power
+353|9|0|0,1,11,-27 dbm
+361|9|0|0,2,15,-25 dbm
+369|9|0|0,3,34,-23 dbm
+377|9|0|0,4,5,-21 dbm
+
+
+# MTKUE 1114 PrachCFID:145 B4 log
+
+SFN/SFN/SLOT/SYMBOL,attempt time,preamble index,Tx power
+193|9|0|0,1,54,-27 dbm
+209|9|0|0,2,7,-25 dbm
+225|9|0|0,3,24,-23 dbm
+
+
+# MTKUE 1114 PrachCFID:145 B4 log
+
+SFN/SFN/SLOT/SYMBOL,attempt time,preamble index,Tx power
+225|9|0|0,1,19,-27 dbm
+241|9|0|0,2,14,-25 dbm
+
+
+"""

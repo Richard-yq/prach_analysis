5477c2a00ae3e4eb2e5ed7f112e12aebc4a80078
Richard-yq <a2311496a@gmail.com>
2025-11-21 14:46:22 +0800

    Add srsRAN testing result 1121

---
+++ 1121/data.py
@@ -0,0 +1,17 @@
+DATA = """
+# MTKUE 1114 PrachCFID:159 B4 log power rammping 4 db
+SFN/SFN/SLOT/SYMBOL,attempt time,preamble index,Tx power
+644|9|1|0,1,2,-30 dbm
+657|9|1|0,2,0,-26 dbm
+669|9|1|0,3,0,-22 dbm
+681|9|1|0,4,2,-18 dbm
+694|9|1|0,5,3,-14 dbm
+696|9|1|0,6,3,-10 dbm
+698|9|1|0,7,1,-6 dbm
+711|9|1|0,8,1,-2 dbm
+724|9|1|0,9,3,2 dbm
+726|9|1|0,10,3,6 dbm
+728|9|1|0,11,1,10 dbm
+740|9|1|0,12,3,18 dbm
+
+"""
